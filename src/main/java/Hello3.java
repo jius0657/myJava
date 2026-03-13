1f6e6e2f100be84b26848414a7ebf53cf49c1dce
jius0657 <jius20060507@gmail.com>
2026-03-13 15:46:08 +0900

    Halloooo3

---
+++ src/main/java/Hello3.java
@@ -0,0 +1,7 @@
+public class Hello3 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello!! My Third Java coding!!");
+        System.out.println("Edit Java Code!!");
+    }
+}
