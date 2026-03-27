b4cd05fa6cb4cb116cc78627e75ed367dc3f99cd
jius0657 <jius20060507@gmail.com>
2026-03-27 14:24:23 +0900

    code03_12 (Scanner)

--- src/main/java/code03_12.java
+++ src/main/java/code03_12.java
@@ -3,10 +3,15 @@ import java.util.Scanner;
 public class code03_12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int num = 99;
+        num = sc.nextInt();
 
         System.out.println( (num > 100) && (num < 200) );
         System.out.println( (num == 99) || (num == 100) );
         System.out.println( ! (num == 100) );
+
+        sc.close();
+
     }
 }
