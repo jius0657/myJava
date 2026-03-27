5f51320d6b8c470393e6bb46f3d85d5382a89b8d
jius0657 <jius20060507@gmail.com>
2026-03-27 13:41:33 +0900

    code03_09

---
+++ src/main/java/code03_09.java
@@ -0,0 +1,13 @@
+public class code03_09 {
+    public static void main(String[] args) {
+        int num = 20;
+        num++ ; System.out.print(num+" ");
+        num-- ; System.out.print(num+" ");
+        num += 3 ; System.out.print(num+" ");
+        num -= 3 ; System.out.print(num+" ");
+        num *= 3 ;
+        System.out.print(num+" ");
+        num /= 3 ; System.out.print(num+" ");
+        num %= 3 ; System.out.print(num+" ");
+    }
+}
