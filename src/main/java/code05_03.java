722b17e2c9b99bd77bb7b437147ff7d03cc6d8b7
jius0657 <jius20060507@gmail.com>
2026-03-31 10:25:04 +0900

    code05_02

---
+++ src/main/java/code05_03.java
@@ -0,0 +1,11 @@
+public class code05_03 {
+    public static void main(String[] args) {
+        int num = 99;
+
+        if (num < 100) {
+            System.out.print("100보다 ");
+            System.out.print("작습니다.");
+        }
+
+    }
+}
