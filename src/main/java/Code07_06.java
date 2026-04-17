9dddca50d3a1b9b24a30ea6e489405817c7aff70
jius0657 <jius20060507@gmail.com>
2026-04-17 15:40:41 +0900

    code07_06(public)

--- src/main/java/Code07_06.java
+++ src/main/java/Code07_06.java
@@ -1,45 +1,37 @@
 class Rabbit {
-    public String shape;
-    public int xPos;
-    public int yPos;
+    private String shape;
+    private int xPos;
+    private int yPos;
 
-    Rabbit() {
-        shape = "토끼";
+    public void setShape(String shape) {
+        this.shape = shape;
     }
 
-    Rabbit(String value) {
-        shape = value;
+    public String getShape() {
+        return this.shape;
     }
 
-    void setposition (int x, int y) {
-        xPos = x;
-        yPos = y;
+    public void setPosition(int x, int y) {
+        this.xPos = x;
+        this.yPos = y;
+    }
+
+    public int getX() {
+        return this.xPos;
+    }
+
+    public int getY() {
+        return this.yPos;
     }
 }
 
 public class Code07_06 {
     public static void main(String[] args) {
-//        Rabbit rabbit1 = new Rabbit();
-//        Rabbit rabbit2 = new Rabbit();
-//        Rabbit rabbit3 = new Rabbit();
-//
-//        rabbit1.shape = "원";
-//        rabbit2.shape = "삼각형";
-//        rabbit3.shape = "토끼";
-
         Rabbit rabbit = new Rabbit();
+        rabbit.setShape("흰토끼");
+        rabbit.setPosition(100, 100);
 
-        rabbit.setposition(100, 200);
-
-//        System.out.printf("토끼 객체의 기본 모양은 [%s]입니다.", rabbit.shape);
-//
-//        Rabbit rabbit1 = new Rabbit("원");
-//        Rabbit rabbit2 = new Rabbit("삼각형");
-//        Rabbit rabbit3 = new Rabbit("토끼");
-//
-//        System.out.printf("토끼1 객체의 모임은 [%s]입니다.\n", rabbit1.shape);
-//        System.out.printf("토끼2 객체의 모임은 [%s]입니다.\n", rabbit2.shape);
-//        System.out.printf("토끼3 객체의 모임은 [%s]입니다.\n", rabbit3.shape);
-
+        System.out.printf("토끼의 모양은 [%s]입니다. \n", rabbit.getShape());
+        System.out.printf("토끼의 위치는 (%d, %d)입니다.\n", rabbit.getX(), rabbit.getY());
     }
 }
