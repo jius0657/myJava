c9c4dbc643690ca40055879b1a79f6fd0f6acbd3
jius0657 <jius20060507@gmail.com>
2026-04-17 15:03:51 +0900

    code07_06

--- src/main/java/Code07_06.java
+++ src/main/java/Code07_06.java
@@ -3,7 +3,15 @@ class Rabbit {
     int xPos;
     int yPos;
 
-    void setPosition(int x, int y) {
+    Rabbit() {
+        shape = "토끼";
+    }
+
+    Rabbit(String value) {
+        shape = value;
+    }
+
+    void setposition (int x, int y) {
         xPos = x;
         yPos = y;
     }
@@ -11,13 +19,25 @@ class Rabbit {
 
 public class Code07_06 {
     public static void main(String[] args) {
-        Rabbit rabbit1 = new Rabbit();
-        Rabbit rabbit2 = new Rabbit();
-        Rabbit rabbit3 = new Rabbit();
+//        Rabbit rabbit1 = new Rabbit();
+//        Rabbit rabbit2 = new Rabbit();
+//        Rabbit rabbit3 = new Rabbit();
+//
+//        rabbit1.shape = "원";
+//        rabbit2.shape = "삼각형";
+//        rabbit3.shape = "토끼";
 
-        rabbit1.shape = "원";
-        rabbit2.shape = "삼각형";
-        rabbit3.shape = "토끼";
+        Rabbit rabbit = new Rabbit();
+
+        System.out.printf("토끼 객체의 기본 모양은 [%s]입니다.", rabbit.shape);
+
+        Rabbit rabbit1 = new Rabbit("원");
+        Rabbit rabbit2 = new Rabbit("삼각형");
+        Rabbit rabbit3 = new Rabbit("토끼");
+
+        System.out.printf("토끼1 객체의 모임은 [%s]입니다.\n", rabbit1.shape);
+        System.out.printf("토끼2 객체의 모임은 [%s]입니다.\n", rabbit2.shape);
+        System.out.printf("토끼3 객체의 모임은 [%s]입니다.\n", rabbit3.shape);
 
     }
 }
