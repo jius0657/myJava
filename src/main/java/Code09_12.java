cf550f2b0d6b7622b66de3567727e25399115bcf
jius0657 <jius20060507@gmail.com>
2026-05-15 10:19:51 +0900

    code09_12

--- src/main/java/Code09_12.java
+++ src/main/java/Code09_12.java
@@ -6,6 +6,7 @@ public class Code09_12 {
         for (int i= 0; i < 3; i++) {
             for (int k = 0; k < 4; k++) {
                 ary[i][k] = value;
+                value++;
             }
         }
         System.out.printf("ary[0][0]부터 ary[2][3]까지 출력 \n");
