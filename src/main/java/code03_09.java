bdaa7a2b0468ce73f278e10dd8f831291bbec3e9
jius0657 <jius20060507@gmail.com>
2026-07-10 12:40:42 +0900

    Code03_09

--- src/main/java/code03_09.java
+++ src/main/java/code03_09.java
@@ -2,12 +2,19 @@ public class code03_09 {
     public static void main(String[] args) {
         int num = 20;
         num++ ; System.out.print(num+" ");
+        //++ = 값 1 증가
         num-- ; System.out.print(num+" ");
+        //-- = 값 1 감소
         num += 3 ; System.out.print(num+" ");
+        // += 3 = num + 3과 동일
         num -= 3 ; System.out.print(num+" ");
+        // -= 3 = num - 3과 동일
         num *= 3 ; System.out.print(num+" ");
+        // *= 3 = num * 3과 동일
         num /= 3 ; System.out.print(num+" ");
+        // /= 3 = num / 3과 동일
         num %= 3 ; System.out.print(num+" ");
+        // %= 3 = num % 3과 동일
 
     }
 }
