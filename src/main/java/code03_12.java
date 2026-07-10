fbf7bba097bbcfed9fc14bdef8fe37641e863243
jius0657 <jius20060507@gmail.com>
2026-07-10 12:50:18 +0900

    Code03_12

--- src/main/java/code03_12.java
+++ src/main/java/code03_12.java
@@ -8,8 +8,11 @@ public class code03_12 {
         num = sc.nextInt();
 
         System.out.println( (num > 100) && (num < 200) );
+        //&& : 그리고(and), 둘다 참이어야 참
         System.out.println( (num == 99) || (num == 100) );
+        // || : 또는(or), 둘 중 하나만 참이어도 참
         System.out.println( ! (num == 100) );
+        // ! : 부정(not), 참이면 거짓/거짓이면 참
 
         sc.close();
 
