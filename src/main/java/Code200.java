d2dc12ec3abdf133b81eee8e5e3ab1e7abe0a47f
jius0657 <jius20060507@gmail.com>
2026-07-10 10:56:16 +0900

    Study

---
+++ src/main/java/Code200.java
@@ -0,0 +1,9 @@
+public class Code200 {
+    public static void main(String[] args) {
+        String a = "12";
+        String b = "34";
+        String result = a + b;
+
+        System.out.println(result);
+    }
+}
