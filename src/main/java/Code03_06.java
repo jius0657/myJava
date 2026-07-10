71c5047ab0fff1e9afde887003110117a013866a
jius0657 <jius20060507@gmail.com>
2026-07-10 12:34:02 +0900

    Study3

---
+++ src/main/java/Code03_06.java
@@ -0,0 +1,10 @@
+public class Code03_06 {
+    public static void main(String[] args) {
+        double num;
+        num = Math.pow(3, 2);
+        System.out.println(num);
+        num = Math.pow(4, 3);
+        System.out.println(num);
+        //Math.pow = 제곱근
+    }
+}
