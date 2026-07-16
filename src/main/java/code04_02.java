7df8f96e6351035b243419e95b062699e9217d09
jius0657 <jius20060507@gmail.com>
2026-07-16 14:40:10 +0900

    Code04_02

--- src/main/java/code04_02.java
+++ src/main/java/code04_02.java
@@ -1,10 +1,15 @@
 public class code04_02 {
     public static void main(String[] args) {
         float f = 0.1234567890123456789012345f;
+//        //float는 실수(소수)를 저장하는자료형
+//        약 7자리 정도만 정확하게 저장할 수 있음
         double d =0.1234567890123456789012345;
+//        //double도 실수를 저장하는 자료형
+//        약 15 ~ 16자리까지 정확하게 저장할 수 있음
 
-        System.out.println(f);
-        System.out.println(d);
+        System.out.println(f); //float에 저장된 값 출력
+        System.out.println(d); //double에 저장된 값 출력
 
     }
 }
+//double이 float보다 더 정확한 실수 값을 저장할 수 있다.
