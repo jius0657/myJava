bca1577133fd897260bf35165b501d4277d098f6
jius0657 <jius20060507@gmail.com>
2026-07-16 14:39:44 +0900

    Code04_04

--- src/main/java/Code04_04.java
+++ src/main/java/Code04_04.java
@@ -1,11 +1,12 @@
 public class Code04_04 {
     public static void main(String[] args) {
-        boolean boo1, boo2;
+        boolean boo1, boo2; //boo1, boo2 두 개의 변수를 만듬
+        //boolean은 참(true) 또는 거짓(false)만 저장하는 자료형
 
-        boo1 = true;
-        System.out.println(boo1);
+        boo1 = true; //true 값을 저장
+        System.out.println(boo1); //저장한 값 출력
 
-        boo2 = (100 < 10);
-        System.out.println(boo2);
+        boo2 = (100 < 10); //100이 10보다 작은지 비교하여 결과 저장
+        System.out.println(boo2); //저장한 값 출력
     }
 }
