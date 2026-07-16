21bfec9e0944e49506203816ac9ce0b3b0f2f45a
jius0657 <jius20060507@gmail.com>
2026-07-16 14:39:54 +0900

    Code04_04

--- src/main/java/code04_03.java
+++ src/main/java/code04_03.java
@@ -1,12 +1,14 @@
 public class code04_03 {
     public static void main(String[] args) {
-        int n1 = 1;
-        int n2 =2;
 
-        System.out.println(n1+n2);
-        System.out.println(n1-n2);
-        System.out.println(n1*n2);
-        System.out.println((double)n1/n2);
+        //변수 선언
+        int n1 = 1; //첫번쨰 정수
+        int n2 = 2; //두번쨰 정수
+
+        System.out.println(n1+n2); //(1 + 2)
+        System.out.println(n1-n2); //(1 - 2)
+        System.out.println(n1*n2); //(1 * 2)
+        System.out.println(n1/n2); //(1 / 2)
 
     }
 }
