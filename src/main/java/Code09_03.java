50234af733470387fc868741029c5e306d744745
jius0657 <jius20060507@gmail.com>
2026-07-28 12:35:03 +0900

    Code09_03

--- src/main/java/Code09_03.java
+++ src/main/java/Code09_03.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class Code09_03 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        //Scanner 객체 생성(cuz 키보드입력)
         int[] numAry = new int[4];
+        //정수 4개를 저장할 수 있는 배열을 만듬
         int hap = 0;
+        //초기화 값
 
         for (int i=0; i<=3; i++) {
+            //for문
             System.out.print("숫자 : ");
             numAry[i] = s.nextInt();
+            //i의 값이 계속 바뀌기 때문에 numAry[i]라고 씀
         }
 
-        for (int i = 0; i <= 3; i++) {
-            hap += numAry[i];
-        }
-
+        hap = numAry[0] + numAry[1] + numAry[2] + numAry[3];
         System.out.println("합계 ==> " + hap);
+
         s.close();
     }
 }
+
